0f99fac0f440f4fe025081772592f78ecb582bf3
KSY1526 <zan05134@naver.com>
2022-03-01 10:06:57 +0900

    back sort!

---
+++ 백준문제풀이/220301/1427.py
@@ -0,0 +1,7 @@
+a = input()
+x = []
+for i in a:
+    x.append(i)
+x.sort(reverse = True)
+for i in x:
+    print(i,end='')
--- 백준문제풀이/220301/2108진행.py
+++ 백준문제풀이/220301/2108진행.py
@@ -1,7 +1,8 @@
-n = int(input())
+import sys
+n = int(sys.stdin.readline()) # 시간초과 방지
 x = []
 for i in range(n):
-    x.append(int(input()))
+    x.append(int(sys.stdin.readline()))
 x.sort()
 print(round(sum(x)/n))
 print(x[n//2])
@@ -13,12 +14,17 @@ swichs = True
 for i in x[1:]:
     if (tem == i):
         count += 1
+        if (count == manycount) and (swichs):
+            # 같은 빈도가 2번째로 카운트 된 경우
+            manys = i
+            swichs = False
     else:
+        if (count > manycount):
+            swichs = True
+            manys = tem
+            manycount = count
+
         count = 1
         tem = i
-    if (count > manycount) and (manys != i):
-        swichs = True
-        manys = i
-
-        
+print(manys)
 print(x[n-1] - x[0])
